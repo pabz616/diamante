2c679f71d5c8b2081c2101aca40a4889ca9386f4
pabz616 <vergara.pablo0616@gmail.com>
2019-12-09 16:17:50 -0500

    Product search tests

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,4 +1,5 @@
 const onDemoSite = require('../locators/demoSiteLocators')
+const demo = require('../test-data/keywords')
 const { I } = inject();
 
 module.exports = {
@@ -8,6 +9,37 @@ module.exports = {
     I.see('New Products For December')
     I.click(onDemoSite.productList.video3);
   },
+
+  searchForValidProduct(){
+    I.fillField(onDemoSite.productSearch.input, demo.valid_keyword);
+    I.click(onDemoSite.productSearch.submit);
+    I.waitForVisible(onDemoSite.productSearch.results.productImage)
+    // I.see(onDemoSite.productSearch.results.productImage);
+    // I.see(onDemoSite.productSearch.results.productName);
+    // I.see(onDemoSite.productSearch.results.productPrice);
+    // I.see(onDemoSite.productSearch.results.productCTA);
+    I.dontSee(onDemoSite.productSearch.results.no_results_msg);
+  },
+
+  searchInvalidProduct(){
+    I.fillField(onDemoSite.productSearch.input, demo.invalid_keyword);
+    I.click(onDemoSite.productSearch.submit);
+    I.waitForVisible(onDemoSite.productSearch.results.productImage)
+    I.see(onDemoSite.productSearch.results.no_results_msg);
+  },
+
+  selectProductAndViewResults(){
+    I.click(onDemoSite.productList.menu);
+    I.click(onDemoSite.productList.o_Fox);
+    I.dontSee(onDemoSite.productList.no_items_found);
+  },
+
+  selectProductWithoutResults(){
+    I.click(onDemoSite.productList.menu);
+    I.click(onDemoSite.productList.o_Canon);
+    I.see(onDemoSite.productList.no_items_found);
+  },
+
   logOut(){
     I.click(onDemoSite.navigation.logout_CTA);
   }
