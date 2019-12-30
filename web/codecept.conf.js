430c09f657ccb48fb9ef17d7320364cf7bc07046
pabz616 <vergara.pablo0616@gmail.com>
2019-12-30 17:06:20 -0500

    Updated the conf.js file

--- web/codecept.conf.js
+++ web/codecept.conf.js
@@ -4,7 +4,19 @@ exports.config = {
   helpers: {
     WebDriver: {
       url: 'http://localhost',
-      browser: 'chrome'
+      smartWait: 5000,
+      timeouts:{
+        "script": 60000,
+        "page load": 10000
+      },
+      browser: 'chrome',
+      restart: 'false',
+      desiredCapabilities:{
+        unexpectedAlertBehaviour: 'dismiss',
+        chromeOptions:{
+          args:[ "--headless", "--disable-gpu", "--window-size=1200,1000", "--no-sandbox" ]
+        }
+      }
     },
   },
   include: {},
